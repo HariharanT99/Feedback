b7b23fdfc4c6c903d72edc68bffcb4d89f2850c4
HariharanT99 <hariharant99@gmail.com>
2021-11-20 00:16:49 +0530

    Validation

--- Presentation/wwwroot/js/FormValidation.js
+++ Presentation/wwwroot/js/FormValidation.js
@@ -1,5 +1,7 @@
 ﻿$('textarea[name="con"]').prop("disabled", true);
 
+
+//Product Category Validation
 $('#cat-error').hide();
 
 let catError = true;
@@ -21,6 +23,8 @@ function validateCategory() {
 }
 
 
+//Product validation
+
 $('#prod-error').hide();
 let prodError = true;
 
@@ -41,9 +45,15 @@ function validateProduct() {
 }
 
 
+//Satisfaction field validation
+
 $('#sat-error').hide();
 
 let satError = true;
+
+$('input[type=radio]').change(function () {
+    validateSatisfactory();
+})
 
 function validateSatisfactory() {
     var isValid = $("input[name=ratingRadioOptions]").is(":checked")
@@ -54,15 +64,14 @@ function validateSatisfactory() {
         satError = false;
     }
     else {
-        if ((value == 4) || (value == 5)) {
-            $('textarea[name="con"]').prop("disabled", false);
-        }
 
         $('#sat-error').hide();
         satError = true;
     }
 }
 
+
+//Comment Validation
 
 $('#pro-error').hide();
 
@@ -84,6 +93,7 @@ function validatePros() {
     }
 }
 
+//Name title validation
 
 $('#title-error').hide();
 
@@ -105,6 +115,7 @@ function validateTitle() {
     }
 }
 
+//Name Validation
 
 $('#name-error').hide();
 
@@ -133,51 +144,213 @@ function validateName() {
     }
 }
 
-$('#inital-error').hide();
+//Initial error
 
+$('#initial-error').hide();
 
 let initialError = true;
 
-$('#name').keyup(function () {
+$('#initial').keyup(function () {
     validateInitial();
 });
 
 function validateInitial() {
-    let nameValue = $('#name').val();
-    if (nameValue.length == '') {
-        $('#name-error').show();
+    let initialValue = $('#initial').val();
+    if (initialValue.length == '') {
+        $('#initial-error').show();
         initialError = false;
     }
     else {
-        $('#name-error').hide();
+        $('#initial-error').hide();
         initialError = true;
     }
 }
 
 
+//Email validation
+$('#email-error').hide();
+
+let emailError = true;
+
+$('#email').keyup(function () {
+    validateEmail();
+})
+
+function validateEmail() {
+    let Value = $('#email').val();
+    var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+    if (Value.length == '') {
+        $('#email-error').show();
+        emailError = false;
+    }
+    else if (!emailReg.test(Value)) {
+        $('#email-error').show();
+        $('#email-error').html
+            ("** enter valid email")
+        emailError = false;
+    }
+    else {
+        $('#email-error').hide();
+        emailError = true;
+    }
+}
+
+//Street validation
+
 $('#street-error').hide();
 
+let streetError = true;
 
+$('#street1').keyup(function () {
+    validateStreet();
+});
+
+function validateStreet() {
+    let Value = $('#street1').val();
+    if (Value.length == '') {
+        $('#street-error').show();
+        streetError = false;
+    }
+    else {
+        $('#street-error').hide();
+        streetError = true;
+    }
+}
+
+//City validation
 
 $('#city-error').hide();
 
+let cityError = true;
 
+$('#city').keyup(function () {
+    validateCity();
+});
+
+function validateCity() {
+    let Value = $('#city').val();
+    if (Value.length == '') {
+        $('#city-error').show();
+        cityError = false;
+    }
+    else {
+        $('#city-error').hide();
+        cityError = true;
+    }
+}
+
+//Region Error
 
 $('#reg-error').hide();
 
+let regionError = true;
+
+$('#region').keyup(function () {
+    validateRegion();
+});
+
+function validateRegion() {
+    let Value = $('#region').val();
+    if (Value.length == '') {
+        $('#reg-error').show();
+        regionError = false;
+    }
+    else {
+        $('#reg-error').hide();
+        regionError = true;
+    }
+}
+
+//Postal code validation
 
 $('#zip-error').hide();
 
+let postalCodeError = true;
 
-$('#count-error').hide();
+$('#postal').keyup(function () {
+    validatePostalCode();
+});
 
-$('#Zip-error').hide();
+function validatePostalCode() {
+    let Value = $('#postal').val();
+    if (Value.length == '') {
+        $('#zip-error').show();
+        postalCodeError = false;
+    }
+    else {
+        $('#zip-error').hide();
+        postalCodeError = true;
+    }
+}
 
-$('#Zip-error').hide();
+//Coutry validation
 
-$('#Zip-error').hide();
+$('#country-error').hide();
 
-$('#Zip-error').hide();
+let countryError = true;
+
+$('#country').change(function () {
+    validateCountry();
+});
+
+function validateCountry() {
+    let Value = $('#country').val();
+    if (Value.length == '') {
+        $('#country-error').show();
+        countryError = false;
+    }
+    else {
+        $('#country-error').hide();
+        countryError = true;
+    }
+}
+
+//File validation
+
+$('#file-error').hide();
+
+$('#fileinput').change(function () {
+    validateFile();
+})
+
+function validateFile() {
+    let file = $('#fileinput');
+    file = file[0].files[0];
+
+    if (file) {
+        if ((file.type.split["/"][1] !== "pdf" || file.type.split["/"][1] !== "png") || (file.size > 500000)) {
+            $('#file-error').show();
+            $('#fileinput').val("");
+        }
+    }
+}
+
+//enable Cons
+
+function ShowUnsatisfiedComment() {
+    $('textarea[name="con"]').prop("disabled", false);
+}
 
 
+
+
+//Submit
+
+$('#submitbtn').click(function () {
+    console.log("Submit")
+
+    validateCategory();
+    validateProduct();
+    validateSatisfactory();
+    validatePros();
+    validateTitle();
+    validateName();
+    validateInitial();
+    validateEmail();
+    validateStreet();
+    validateCity();
+    validateRegion();
+    validatePostalCode();
+    validateCountry();
+})
 
